603ef1177f6595f7105fda4ca9149a2bf05b88a9
onejun525 <onejun525@gmail.com>
2025-10-16 11:09:54 +0900

    회원탈퇴 api 생성

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Enum, Boolean
 import datetime
 from database import Base
 
@@ -12,3 +12,4 @@ class User(Base):
     email = Column(String(255), unique=True, index=True, nullable=False)
     gender = Column(Enum("여성", "남성", name="gender_enum"), nullable=True)
     create_date = Column(DateTime, default=datetime.datetime.now)
+    is_deleted = Column(Boolean, default=False)  # 소프트 딜리트용 필드 추가
